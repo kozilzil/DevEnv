9670aad11b62892885d19032220f8872d1571085
hdkim <hdkim@canvasbio.com>
2019-09-06 09:58:22 +0900

    Spacemacs init

---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,13 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  ("--no-ext-diff"))
+ (magit-dispatch nil)
+ (magit-log
+  (("--" "micro_keypad/python_test/main.py")))
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-remote
+  ("-f"))
+ (magit-stash nil))
